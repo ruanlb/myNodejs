fc73c48992542be53fc099f2842022dfd1f1a6cd
ruanlb <ruanlb@foxmail.com>
2016-07-23 15:42:58 +0800

    v 0.1.2

---
+++ test001/apiController.js
@@ -0,0 +1,80 @@
+// 调用api
+exports.callApi = function(req, res){
+    var myResource = req.params.resource; // 资源名称
+    var myAction = req.method; // 访问资源的方法
+    var myId = req.params.id; // 资源id
+    var myApi = new Object(); // 定义要调用的api对象
+    var myResult = new Object(); // 定义返回对象
+    // 加载指定api模块
+    try {
+        myApi = require('./api/' + myResource);
+    }
+    catch(e) {
+        myResult.code = -1001;
+        myResult.message = '你指定的资源类型[' + myResource + ']不存在！';
+        res.status(404).json(myResult); // 返回结果
+        return;
+    }
+    // 根据携带的参数调用对应的方法
+    try {
+        exports.doAction(myApi, req, res);
+    }
+    catch(e) {
+        myResult.code = -1002;
+        myResult.message = '你指定的资源类型[' + myResource + ']不支持[' + myAction + ']方法！';
+        res.status(404).json(myResult); // 返回结果
+        return;
+    }
+    res.json(myResult); // 返回结果
+};
+// 执行api的方法
+exports.doAction = function(iApi, req, res){
+    var myResource = req.params.resource; // 资源名称
+    var myAction = req.method; // 访问资源的方法
+    var myId = req.params.id; // 资源id
+    var myApi = iApi; // 定义要调用的api对象
+    var myResult = new Object(); // 定义返回对象
+    if(myId) {
+        // 有Id的访问
+        if(myAction == 'GET') {
+            // 单资源查询
+            myResult = myApi.getOne(myId);
+        }
+        else if(myAction == 'put') {
+            // 添加指定Id的资源
+            myResult = myApi.add(req.body, myId);
+        }
+        else if(myAction == 'Post') {
+            // 修改指定Id的资源
+            myResult = myApi.modify(myId);
+        }
+        else if(myAction == 'delete') {
+            // 删除指定资源
+            myResult = myApi.modify(myId);
+        }
+    }
+    else {
+        // 无Id的访问
+        if(req.method == 'GET') {
+            // 多条件查询
+            myResult = myApi.getSome(req.query);
+        }
+        else if(myAction == 'put') {
+            // 添加资源，由系统分配Id
+            myResult = myApi.add(req.body);
+        }
+        else if(myAction == 'Post') {
+            // 批量修改资源
+            myResult = myApi.modifySome(req.query, req.body);
+        }
+        else if(myAction == 'delete') {
+            // 批量删除资源
+            myResult = myApi.deleteSome(req.query, req.body);
+        }            
+    }
+    return myResult;
+};
+// 注册过滤器
+exports.registerFilter = function(iFilterName){
+    
+};
--- test001/routes.js
+++ test001/routes.js
@@ -1,40 +1,12 @@
 // 注册api路由到Web应用
 exports.routeApi = function(iWebApp){
-    // 加载api管理器
-    var myApiManager = require('./apiManager');
     // 创建api的基础方法
-    // get--获取指定单个资源的信息
-    iWebApp.get('/api/:resource/:id', function (req, res) {
-        var myResource = req.params.resource; // 资源名称
-        var myId = req.params.id; // 资源id
-        var myApi = new Object(); // 定义要调用的api对象
-        var myResult = new Object(); // 定义返回对象
-        try {
-            myApi = require('./api/' + myResource);
-        }
-        catch(e) {
-            myResult.code = -404;
-            myResult.message = '你查询的资源类型[' + myResource + ']不存在!';
-            res.send(JSON.stringify(myResult)); // 返回结果
-            return;
-        }
-        var myResult = myApi.getOne(myId); // 调用单个资源查询的方法
-        res.send(JSON.stringify(myResult)); // 返回结果
-    });
-    // get--获取指定的一批资源的信息
-    iWebApp.get('/api/:resource', function (req, res) {
-        res.send('你的资源名称是：' + req.params.resource);
-    });
-    // post--添加资源
-    iWebApp.post('/api/:resource', function (req, res) {
-        res.send('你添加了一个【' + resource + '】资源。');
-    });
-    // put--修改资源
-    iWebApp.put('/api/:resName/:resId', function (req, res) {
-        res.send('你修改了Id为【' + resId + '】的' + resId + '资源。');
-    });
-    // delete--删除资源
-    iWebApp.delete('/api/:resName/:resId', function (req, res) {
-        res.send('你删除了Id为【' + resId + '】的【' + resId + '】资源。');
+    iWebApp.all('/api/:resource/:id?', function (req, res) {
+        // 调用api基础控制器
+        var myApiController = require('./apiController');
+        // 通过api基础控制器调用对应的api
+        var myResult = myApiController.callApi(req, res);
+        // 返回结果
+        return myResult;
     });
 };
